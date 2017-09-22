2d7e87690ae821579b34924b95d7aa2d574e429d
idawatibustan <idawati.bustan@gmail.com>
2017-09-22 04:38:06 +0000

    Added confusion matrix plot

--- software_cp/nn.py
+++ software_cp/nn.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
+import itertools
 import matplotlib.pyplot as plt 
 import numpy as np
 import pandas as pd
 import sklearn.neural_network as nn
+from sklearn.metrics import confusion_matrix
 
 # Load training and testing dataset
 train = pd.read_csv('ds_train.csv')
@@ -35,3 +37,22 @@ plt.show()
 
 print("Training set score for ADAM: %f" % mlp.score(trainData, trainLabel))
 print("Test set score for ADAM: %f"     % mlp.score(testData , testLabel ))
+
+# Produce confusion matrix
+pred = mlp.predict(testData)
+
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(testLabel, pred)
+np.set_printoptions(precision=2)
+
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=[3,4,5],
+                      title='Confusion matrix, without normalization')
+
+# Plot normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=[3,4,5], normalize=True,
+                      title='Normalized confusion matrix')
+
+plt.show()
