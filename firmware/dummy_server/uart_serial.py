a113c1c9de11443c4849c33b79df4a5dd4050129
Jeston Teo <jestontzh@gmail.com>
2017-10-13 16:49:39 +0800

    uart_serial.py now outputs to a file as well. Edited just_sending.ino

--- firmware/dummy_server/uart_serial.py
+++ firmware/dummy_server/uart_serial.py
@@ -18,11 +18,19 @@ try:
             read_sensor_flag = 1
 
     # use sys clock to get time frame
+    timestr = time.strftime("%Y%m%d%H%M%S")
+    filename = "datarec_" + timestr + ".csv"
+    data_file = open(filename, "a")
+    data_file.write("ax0,ay0,az0,gx0,gy0,gz0,ax1,ay1,az1,gx1,gy1,gz1\n")
     while read_sensor_flag:
         received_string = ser.readline()
-        print(received_string)
+        if (len(received_string)>10):
+            data_file.write(received_string)
+            print(received_string)
+
 except KeyboardInterrupt:
     print("\nClosing port /dev/ttyAMA0")
     ser.write("4".encode(encoding='utf_8'))
     time.sleep(2)
+    data_file.close()
     ser.close()
