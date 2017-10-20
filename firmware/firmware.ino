07912f94bbff34c602f86b5080063a2edb5bde5b
nicholasluimy <nicholasluimy@gmail.com>
2017-10-20 15:56:04 +0800

    finally figured out the bug, fixed char array overflow

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,148 +1,262 @@
 #include <Arduino_FreeRTOS.h>
-#define LONG_TIME 0xffff
-#define CIRCULAR_BUFFER_XS
-#include <CircularBuffer.h>
+#include <task.h>
+#include <semphr.h>
+#include <Wire.h>
 
-// variables for polling_sensors_task
-#include<Wire.h>
-const int N = 2;
-const int MPU_addr[N]={0x68, 0x69};  // I2C address of the first MPU-6050
-// const int MPU_addr2=0x69;  // I2C address of the second MPU-6050
-int16_t AcX[N],AcY[N],AcZ[N],Tmp[N],GyX[N],GyY[N],GyZ[N];
-int i;
+SemaphoreHandle_t buffer_semaphore = NULL;
 
-// variables for tx_rpi_task
+const int MPU_1_address = 0x68;
+const int MPU_2_address = 0x69;
+const int N  = 2;
+static int16_t AcX[N], AcY[N], AcZ[N], Tmp[N], GyX[N], GyY[N], GyZ[N];
+
 static int handshake_flag = 0;
 static int send_sensor_data = 0;
-static char incoming = 'x';
-
-// dataframe object
 
 struct Dataframe {
-    int16_t AcX[N],AcY[N],AcZ[N],Tmp[N],GyX[N],GyY[N],GyZ[N];
-};
+  int16_t AcX1, AcY1, AcZ1, Tmp1, GyX1, GyY1, GyZ1, AcX2, AcY2, AcZ2, Tmp2, GyX2, GyY2, GyZ2;
+} dataframe;
 
-// variables for dataframe buffers. 100 because max polling is 40hz, buffer for 1s
-static CircularBuffer<Dataframe,5> dataframe_buffer;
+class DataframeBuffer {
+  private:
+    int _front, _back, _count, _max;
+    Dataframe _buffer[100];
+  public:
+    DataframeBuffer() {
+      _front = 0;
+      _back = 0;
+      _count = 0;
+      _max = 100;
 
-void setup()
-{  
-    // setup for polling_sensors_task
-    Wire.begin();
-    Serial.begin(57600);
-    Serial1.begin(57600);
-    for (i = 0; i < N; i++){
-        Wire.beginTransmission(MPU_addr[i]);
-        Wire.write(0x6B);  // PWR_MGMT_1 register
-        Wire.write(0);     // set to zero (wakes up the MPU-6050)
-        Wire.endTransmission(true);
     }
 
-    xTaskCreate(polling_sensors_task, "polling_sensors_task", 300, NULL, 2, NULL);
-    xTaskCreate(tx_rpi_task, "tx_rpi_task", 300, NULL, 1, NULL);
-    
-}
-
-
-void loop()
-{ 
-}
-
-// polls sensors every xDelay interval, and writes values to dataframe_buffer
-void polling_sensors_task(void * pvParameters)
-{
-    // execute at 25hz
-    const TickType_t xDelay = 500/ portTICK_PERIOD_MS;
-    for (;;) {
-      Dataframe dataframe_poll = Dataframe();
-    
-    for (i = 0; i < N; i++){
-        Wire.beginTransmission(MPU_addr[i]);
-        Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-        Wire.endTransmission(false);
-        Wire.requestFrom(MPU_addr[i],14,true);  // request a total of 14 registers
-        dataframe_poll.AcX[i]=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
-        dataframe_poll.AcY[i]=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-        dataframe_poll.AcZ[i]=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-        dataframe_poll.Tmp[i]=round((Wire.read()<<8|Wire.read())/340.00+36.53);  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-        dataframe_poll.GyX[i]=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-        dataframe_poll.GyY[i]=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-        dataframe_poll.GyZ[i]=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+    bool isEmpty() {
+      return _count == 0;
     }
 
-    //write to buffer
-    dataframe_buffer.push(dataframe_poll);
-    Serial.print("poll frame size");
-    Serial.println(dataframe_buffer.size());
-    //repeat at fixed interval for 25hz 
-    Serial.println("Pushed");
-    vTaskDelay(xDelay);
+    int size() {
+      return _count;
     }
+
+    // add entry to back of buffer
+    void push(Dataframe dataframe) {
+      int next = (_back + 1) % _max;
+      if (_count < _max) {
+        _count += 1;
+        _buffer[next] = dataframe;
+        _back = next;
+      } else {
+        _buffer[next] = dataframe;
+        _back = next;
+        _front = (_front + 1) % _max;
+      }
+    }
+
+    // retrieve and remove frontmost entry
+    // use with isEmpty check, does not handle empty buffer case
+    Dataframe shift() {
+      Dataframe dataframe = _buffer[_front];
+      _front = (_front + 1) % _max;
+      _count -= 1;
+      return dataframe;
+    }
+} dataframe_buffer;
+
+
+
+void setup() {
+  Wire.begin();
+  Serial.begin(74880);
+  Serial1.begin(74880);
+  // wake both MPUs
+  Wire.beginTransmission(MPU_1_address);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  Wire.beginTransmission(MPU_2_address);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+
+  buffer_semaphore = xSemaphoreCreateMutex();
+  //Serial.println("created buffer_semaphore");
+  xSemaphoreGive(buffer_semaphore);
+  //Serial.println("gave buffer_semaphore in setup");
+
+  xTaskCreate(read_mpu_values_task, "read_mpu_values", 300, NULL, 3, NULL);
+  xTaskCreate(comms_task, "comms", 300, NULL, 2, NULL);
+  //Serial.println("tasks created");
+
 }
 
+void setupMPUPollng(int address) {
+  Wire.beginTransmission(address);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(address, 14, true); // request a total of 14 registers
+}
+
+void pollMPU(int i) {
+    AcX[i]=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+    AcY[i]=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+    AcZ[i]=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+    Tmp[i]=round((Wire.read()<<8|Wire.read())/340.00+36.53);  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+    GyX[i]=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+    GyY[i]=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+    GyZ[i]=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+// fake data for when testing with only arduino
+//  AcX[i] = 1 * i; // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+//  AcY[i] = 2 * i; // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+//  AcZ[i] = 3 * i; // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+//  Tmp[i] = 4 * i; // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+//  GyX[i] = 5 * i; // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+//  GyY[i] = 6 * i; // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+//  GyZ[i] = 7 * i; // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+}
+
+void read_mpu_values_task(void * pvParameters) {
+  // read at 25hz, so 40ms delay. upgrade to delayuntil later
+  const TickType_t xDelay = 40 / portTICK_PERIOD_MS;
+  while (1) {
+    if (buffer_semaphore != NULL) {
+      if (xSemaphoreTake(buffer_semaphore, portMAX_DELAY) == pdTRUE) {
+        //Serial.println("in poll task");
+        setupMPUPollng(MPU_1_address);
+        pollMPU(0);
+        setupMPUPollng(MPU_2_address);
+        pollMPU(1);
+        dataframe.AcX1 = AcX[0];
+        dataframe.AcY1 = AcY[0];
+        dataframe.AcZ1 = AcZ[0];
+        dataframe.Tmp1 = Tmp[0];
+        dataframe.GyX1 = GyX[0];
+        dataframe.GyY1 = GyY[0];
+        dataframe.GyZ1 = GyZ[0];
+        dataframe.AcX2 = AcX[1];
+        dataframe.AcY2 = AcY[1];
+        dataframe.AcZ2 = AcZ[1];
+        dataframe.Tmp2 = Tmp[1];
+        dataframe.GyX2 = GyX[1];
+        dataframe.GyY2 = GyY[1];
+        dataframe.GyZ2 = GyZ[1];
+
+        dataframe_buffer.push(dataframe);
+        //Serial.print("dataframe buffer size: ");Serial.println(dataframe_buffer.size());
+        xSemaphoreGive(buffer_semaphore);
+        vTaskDelay(xDelay);
+      }
+    }
+  }
+}
 
 void tx_dataframe_to_rpi()
 {
-    // buffer checks to prevent access of empty buffer
-    Serial.print("dataframe_buffer size");
-    Serial.println(dataframe_buffer.size());
-    while (!dataframe_buffer.isEmpty()) {
-        Serial.print("dataframe_buffer size IN WHILE");
-        Serial.println(dataframe_buffer.size());
+  if (buffer_semaphore != NULL) {
+    if (xSemaphoreTake(buffer_semaphore, portMAX_DELAY) == pdTRUE) {
+      //Serial.println(dataframe_buffer.isEmpty());
+      //Serial.print("buffer size:  ");Serial.println(dataframe_buffer.size());
+      while (!dataframe_buffer.isEmpty()) {
+        //Serial.println("inside dataframe is not empty");
         Dataframe dataframe_tx = dataframe_buffer.shift();
-        char sensor_one[100];
-        char sensor_two[100];
-        sprintf(sensor_one, "%d %d %d %d %d %d %d", MPU_addr[0], dataframe_tx.AcX[0], dataframe_tx.AcY[0], dataframe_tx.AcZ[0], dataframe_tx.GyX[0], dataframe_tx.GyY[0], dataframe_tx.GyZ[0]);
-        sprintf(sensor_two, "%d %d %d %d %d %d %d", MPU_addr[1], dataframe_tx.AcX[1], dataframe_tx.AcY[1], dataframe_tx.AcZ[1], dataframe_tx.GyX[1], dataframe_tx.GyY[1], dataframe_tx.GyZ[1]);
-        Serial.println(sensor_one);
-        Serial.println(sensor_two);
+        char output[2000];
+        sprintf(output, "%d %d %d %d %d %d %d %d %d %d %d %d",
+                dataframe_tx.AcX1, dataframe_tx.AcY1, dataframe_tx.AcZ1, dataframe_tx.GyX1, dataframe_tx.GyY1, dataframe_tx.GyZ1,
+                dataframe_tx.AcX2, dataframe_tx.AcY2, dataframe_tx.AcZ2, dataframe_tx.GyX2, dataframe_tx.GyY2, dataframe_tx.GyZ2);
+        Serial1.println(output);
+        //Serial.println(output);
+      }
+      //Serial.println("before give semaphore");
+      xSemaphoreGive(buffer_semaphore);
+      //Serial.println("after give semaphore");
     }
-   
+  }
+
 }
 
-// performs handshake to acknowledge that rpi is ready, serial status checks
-// calls tx_dataframe_to_rpi if checks are valid
-void tx_rpi_task(void * pvParameters)
-{   
-  const TickType_t xDelay = 1500 / portTICK_PERIOD_MS;
-  for (;;) {
-    Serial.println("tx_task");
-    Serial.println(incoming);
-    Serial.print("sen_sensor_data");
-    Serial.println(send_sensor_data);
-    Serial.print("handshake_flag");
-    Serial.println(handshake_flag);
-    if (handshake_flag == 0) {    
-        while (Serial1.available() > 0) {
-          Serial.println("in handshake");
-            char received = Serial1.read();
-            incoming = received;
-            if (incoming == '1') {
-                Serial1.println('2');
-                Serial.println('2');
-                incoming = 'x';
-            } else if (incoming == '3') {
-                handshake_flag = 1;
-                send_sensor_data = 1;
-                incoming = 'x';
-            }
+void comms_task(void * pvParameters) {
+  //Serial.println("entered comms task");
+  // try this every 430ms, which is about the time it takes for 10 readings (40ms delay between readings)
+  const TickType_t xDelay = 430 / portTICK_PERIOD_MS;
+  while (1) {
+//    Serial.print("handshake_flag: "); Serial.println(handshake_flag);
+//    Serial.print("send_sensor_data" ); Serial.println(send_sensor_data);
+    if (handshake_flag == 0) {
+//      Serial.println("in comms task");
+      while (Serial.available() > 0) {
+        char inc = Serial.read();
+        if (inc == '1') {
+          Serial1.println('2');
+        } else if (inc == '3') {
+//          Serial.print("RECEIVED A 3 SETTING FLAGS! handshake: ");
+          handshake_flag = 1;
+          send_sensor_data = 1;
+//          Serial.print(handshake_flag);
+//          Serial.print("  send_sensor_data: "); Serial.println(send_sensor_data);
         }
+      }
     }
     if (send_sensor_data == 1) {
-        Serial.println("before tx");
-        tx_dataframe_to_rpi();
-        while (Serial1.available() > 0) {
-          //Serial.println("in tx");
-            char received = Serial1.read();
-            incoming = received;
-            
-        }
+      tx_dataframe_to_rpi();
     }
+
     vTaskDelay(xDelay);
   }
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void loop() {
+
+}
 
 
 
