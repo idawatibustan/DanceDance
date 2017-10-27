fca6b6142a9bda48057c60cefefeff4e35f8c08a
Jeston Teo <jestontzh@gmail.com>
2017-10-27 16:39:48 +0800

    Added checksum checking

--- firmware/dummy_server/uart_serial.py
+++ firmware/dummy_server/uart_serial.py
@@ -1,36 +1,53 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/ttyAMA0', 57600)
+def verify_checksum(received_string):
+    result_list = received_string.split(",")
+    checksum_value = float(result_list[-1])
+    total = 0
+    return_string = ""
+    for values in result_list[0:len(result_list)-1]:
+        total = total + float(values)
+        return_string = return_string + values + " "
+    return total == checksum_value, return_string
 
-handshake_flag = 1
-read_sensor_flag = 0
+def main():
+    ser = serial.Serial('/dev/ttyAMA0', 57600)
+    handshake_flag = 1
+    read_sensor_flag = 0
 
-try:
-    print("Initialising handshake sequence")
-    while handshake_flag:
-        ser.write("1".encode(encoding='utf_8'))
-        response = ser.readline() # this is blocking
-        if response.rstrip() == "2":
-            print("Received ACK2, sending confirmation ACK3")
-            ser.write("3".encode(encoding='utf_8'))
-            handshake_flag = 0
-            read_sensor_flag = 1
+    try:
+        print("Initialising handshake sequence")
+        while handshake_flag:
+            ser.write("1".encode(encoding='utf_8'))
+            response = ser.readline() # this is blocking
+            if response.rstrip() == "2":
+                print("Received ACK2, sending confirmation ACK3")
+                ser.write("3".encode(encoding='utf_8'))
+                handshake_flag = 0
+                read_sensor_flag = 1
 
-    # use sys clock to get time frame
-    timestr = time.strftime("%Y%m%d%H%M%S")
-    filename = "datarec_" + timestr + ".csv"
-    data_file = open(filename, "a")
-    data_file.write("ax0,ay0,az0,gx0,gy0,gz0,ax1,ay1,az1,gx1,gy1,gz1\n")
-    while read_sensor_flag:
-        received_string = ser.readline()
-        if (len(received_string)>10):
-            data_file.write(received_string)
-            print(received_string)
+        # use sys clock to get time frame
+        timestr = time.strftime("%Y%m%d%H%M%S")
+        filename = "datarec_" + timestr + ".csv"
+        data_file = open(filename, "a")
+        data_file.write("ax0,ay0,az0,gx0,gy0,gz0,ax1,ay1,az1,gx1,gy1,gz1\n")
+        while read_sensor_flag:
+            received_string = ser.readline()
+            is_verified, processed_string = verify_checksum(received_string)
+            print(is_verified)
+            if (is_verified):
+                if (len(processed_string)>10):
+                    data_file.write(processed_string)
+                    print(processed_string)
+            else:
+                print("SOMETHING IS WRONG WITH THE DATA YOU SENT, DATA DUMPED")
 
-except KeyboardInterrupt:
-    print("\nClosing port /dev/ttyAMA0")
-    ser.write("4".encode(encoding='utf_8'))
-    time.sleep(2)
-    data_file.close()
-    ser.close()
+    except KeyboardInterrupt:
+        print("\nClosing port /dev/ttyAMA0")
+        ser.write("4".encode(encoding='utf_8'))
+        time.sleep(2)
+        data_file.close()
+        ser.close()
+
+main()
