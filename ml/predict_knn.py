9d11d94d1371e2a99d0f5032a2d85ec3eb4c59ad
idawatibustan <idawati.bustan@gmail.com>
2017-10-27 13:36:37 +0800

    Added value error check for the predict_knn

--- ml/predict_knn.py
+++ ml/predict_knn.py
@@ -3,6 +3,7 @@ import preprocess
 import timeseries as ts
 import pickle
 import pandas as pd
+import numpy as np
 from sklearn.neighbors import KNeighborsClassifier as knn
 from sklearn.metrics import confusion_matrix
 from plot_helper import plot_confusion_matrix
@@ -10,19 +11,25 @@ from plot_helper import plot_confusion_matrix
 csvfile = 'test.csv'
 df = pd.read_csv(csvfile)
 
+def is_clean(df_):
+    if np.any(pd.isnull(df_)):
+        return False
+    return True
+
 def process_data(window):
     # Do low pass filter here
     feats = ts.extract_feature( window )
-    # feats['label'] = label
+    if not is_clean(feats):
+        raise ValueError("NaN data found, window can't be processed")
     return feats
 
-
-
-def main():
-    # f = pd.DataFrame()
-    f = process_data(df)
-    loaded_knn = pickle.load(open('first_activities.knn','rb'))
-    result = loaded_knn.predict(f)
+    try:
+        f = process_data(df)
+    except ValueError as e:
+        print "ValueError:", e
+        return
+    loaded_knn = pickle.load(open('classifier/activities_kf.knn','rb'))
+    result = loaded_knn.predict_proba(f)
     print result
     # ext_data = process_data(window)
     # y_pred = first_activities.predict(testData)
