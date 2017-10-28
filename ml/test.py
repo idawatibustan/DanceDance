c758924a2d5e837fe444edbf3a1c5a70321f327c
kokwoo <wongkokwoo@gmail.com>
2017-10-28 17:24:36 +0800

    edited test.py

--- ml/test.py
+++ ml/test.py
@@ -1,22 +1,27 @@
 import uart_serial_v3 as uart
 import time
+import threading
+
+dance_moves = ['idle', 'wavehands', 'busdriver', 'frontback', 'sidestep', 'jumping', 'jumpingjack', 'turnclap', 'squartturnclap', 'window', 'window360', 'prediction error', 'not ready yet']
 
 if __name__=="__main__":
     us = uart.Uart_Serial(print_sensor=False)
     try:
         while True:
             res = us.get_prediction()
-            if not res == 11:
-                print res
+            # if not res == 11:
+            print dance_moves[res]
             time.sleep(1)
             if not us._collect_thread.isAlive():
                 us._collect_thread = threading.Thread(
-                    target=self.start_reading,
+                    target=us.start_reading,
                     args=()
                     )
                 us._collect_thread.start()
     except KeyboardInterrupt:
         us.shutdown()
+        exit()
     except Exception as e:
         print "Exception caught:", str(e)
         us.shutdown()
+        exit()
