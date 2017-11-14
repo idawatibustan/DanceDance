42690bea41a3f236193626210decf2346564a728
idawatibustan <idawati.bustan@gmail.com>
2017-11-14 20:25:51 +0800

    Updated train10 feature selection

--- ml/train10_kf_fs_knn_v3.py
+++ ml/train10_kf_fs_knn_v3.py
@@ -10,7 +10,7 @@ import numpy as np
 import pickle
 import itertools
 
-TRAINING_FILE="data_extracted_v3_1711131701"
+TRAINING_FILE="data_extracted_v3_1711141834"
 TRAINING_FILEPATH='dataset/data_ext/'
 TESTING_FILE="data_test_extracted_v3_1711110156"
 TESTING_FILEPATH='dataset/data_ext_test/'
@@ -180,9 +180,11 @@ def kfoldcrossval(df, k):
 
 def test_model(knnModel):
     df = pd.read_csv(TRAINING_FILEPATH+TRAINING_FILE+'.csv')
-    df = df[feature_list]
+    if fs:
+        df = df[feature_list]
     """EXCLUDE DANCE MOVES"""
-    # df = df.loc[df['label'] < 6]
+    if ex:
+        df = df.loc[df['label'] != exclusion]
     cnf_mats = []
     for i in range(1,9):
         print("Testing on excluded dancer:", i)
@@ -192,7 +194,10 @@ def test_model(knnModel):
     plot_all_cnfmats(cnf_mats)
 
     df = pd.read_csv(TESTING_FILEPATH+TESTING_FILE+'.csv')
-    df = df[feature_list]
+    if fs:
+        df = df[feature_list]
+    if ex:
+        df = df.loc[df['label'] != exclusion]
     print("Testing on unseen dancer")
     cm, score = test_classifier(knnModel, df)
     plot_cnfmats(cm)
@@ -217,7 +222,7 @@ if __name__ == "__main__":
     'corr_gxy0', 'corr_ax1x0', 'corr_ax1y0', 'corr_ax1z0', 'corr_ay1x0',
     'corr_ay1y0', 'corr_ay1z0', 'corr_gy0y1',
     'label','dancer','collection']
-    """ FEATURES 10 moves """
+    """ FEATURES 10 moves - best n_neighbor = 4 """
     feature_list = [
     'mean_ax0', 'mean_az0', 'mean_ax1', 'mean_az1', 'mean_gy1',
     'mean_abs_ax0', 'mean_abs_ay0', 'mean_abs_az0', 'mean_abs_ax1',
@@ -235,6 +240,25 @@ if __name__ == "__main__":
     'corr_ax1x0', 'corr_ay1x0', 'corr_ay1y0', 'corr_ay1z0', 'corr_az1x0',
     'label','dancer','collection']
 
+    """ FEATURES 10 moves V4 - Best n-neighbor = 4 """
+    feature_list = [
+    'mean_ax0', 'mean_az0', 'mean_ax1', 'mean_az1', 'mean_gx1',
+    'mean_gy1', 'mean_abs_ax0', 'mean_abs_az0', 'mean_abs_ax1',
+    'mean_abs_ay1', 'mean_abs_az1', 'mean_abs_gx1', 'mean_abs_gy1',
+    'std_ax0', 'std_ay0', 'std_az0', 'std_ax1', 'std_ay1', 'std_az1',
+    'std_gx0', 'std_gy0', 'std_gx1', 'std_gy1', 'median_ax0',
+    'median_az0', 'median_ax1', 'median_az1', 'median_gy1', 'mad_ax0',
+    'mad_ay0', 'mad_az0', 'mad_ax1', 'mad_ay1', 'mad_az1', 'mad_gx0',
+    'mad_gy0', 'mad_gx1', 'mad_gy1', 'max_ax0', 'max_az0', 'max_ax1',
+    'max_ay1', 'max_az1', 'max_gx0', 'max_gy0', 'max_gy1', 'min_ax0',
+    'min_ay0', 'min_az0', 'min_ax1', 'min_az1', 'min_gx0', 'min_gy0',
+    'min_gx1', 'min_gy1', 'range_ay0', 'range_az0', 'range_ax1',
+    'range_ay1', 'range_az1', 'range_gx0', 'range_gy0', 'range_gx1',
+    'range_gy1', 'corr_axz0', 'corr_ayz0', 'corr_axz1', 'corr_gxy0',
+    'corr_gxy1', 'corr_ax1x0', 'corr_ay1x0', 'corr_ay1y0', 'corr_ay1z0',
+    'corr_az1x0', 'corr_gx0x1',
+    'label','dancer','collection']
+
     """ GET ALL COLUMNS """
     df2 = pd.read_csv(TRAINING_FILEPATH+TRAINING_FILE+'.csv')
 
@@ -242,16 +266,23 @@ if __name__ == "__main__":
     # df = df2.loc[df2['dancer'] != EXC_DANCER]
 
     """ EXCLUDING SOME LABELS """
-    # df = df2.loc[df2['label'] < 6]
-    df = df2
+    ex = True
+    exclusion = 12
+    if ex:
+        df = df2.loc[df2['label'] != exclusion]
+    else:
+        df = df2
 
     """ REMOVE UNDESIRED COLUMNS """
-    df = df[feature_list]
+    fs = True
+    if fs:
+        if fs:
+            df = df[feature_list]
     print("Number of features", len(df.columns))
 
     neighbors = range(1,20)
 
-    k = 5
+    k = 4
     mf, bestModel = kfoldcrossval(df, k)
     test_model(bestModel)
     plt.show()
