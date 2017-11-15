7633066d9389cf17c66755374de3be74f4d5df80
idawatibustan <idawati.bustan@gmail.com>
2017-11-15 17:26:37 +0800

    Updated training neighbor test

--- ml/train10_kf_fs_knn_v3_test.py
+++ ml/train10_kf_fs_knn_v3_test.py
@@ -5,12 +5,13 @@ from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import KFold
 from plot_helper import plot_confusion_matrix
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 import numpy as np
 import pickle
 import itertools
 
-TRAINING_FILE="data_extracted_v3_1711131701"
+TRAINING_FILE="data_extracted_v3"
 TRAINING_FILEPATH='dataset/data_ext/'
 TESTING_FILE="data_test_extracted_v3_1711110156"
 TESTING_FILEPATH='dataset/data_ext_test/'
@@ -22,7 +23,7 @@ if not exists('log'):
 if isfile('log/'+MODEL_FILE+'.log'):
     remove('log/'+MODEL_FILE+'.log')
 
-def train_knn(train_set, test_set, k):
+def train_knn(train_set, test_set, k, algo, wgt):
     droplabels = ['label', 'dancer', 'collection']
     trainData  = train_set.drop(droplabels, axis=1).values
     trainLabel = train_set.label.values
@@ -30,8 +31,7 @@ def train_knn(train_set, test_set, k):
     testData  = test_set.drop(droplabels, axis=1).values
     testLabel = test_set.label.values
     
-    # knnclf = knn(n_neighbors=k)    
-    knnclf = knn(n_neighbors=k , algorithm='ball_tree' , leaf_size=25 , n_jobs=2 , weights='distance')
+    knnclf = knn(n_neighbors=k, algorithm=algo, n_jobs=2 , weights=wgt)
 
     knnModel = knnclf.fit(trainData , trainLabel)
 
@@ -139,7 +139,7 @@ def activate_logger(name, filename, append=True):
 def kfoldcrossval(df, k):
     mf = pd.DataFrame()
     count = 0
-    for i in range(3):
+    for i in range(1):
         # kf declare
         kf = KFold( n_splits=5, shuffle=True, random_state=None )
         for train_i, test_i in kf.split(df):
@@ -155,7 +155,7 @@ def kfoldcrossval(df, k):
                 log.warning("train/test: %d, %d" % ( len(train_l), len(test_l) ) )
 
             # get training model & conf
-            res = train_knn(train, test, k)
+            res = train_knn(train, test, k, 'ball_tree', 'distance')
             
             # add model & conf to table
             line = pd.DataFrame( data=res, index=[0] )
@@ -179,19 +179,20 @@ def kfoldcrossval(df, k):
     return mf, knnModel
 
 def test_model(knnModel):
-    df = pd.read_csv(TRAINING_FILEPATH+TRAINING_FILE+'.csv')
-    df = df[feature_list]
-    """EXCLUDE DANCE MOVES"""
-    # df = df.loc[df['label'] < 6]
-    cnf_mats = []
-    for i in range(1,9):
-        print("Testing on excluded dancer:", i)
-        df1 = df.loc[df['dancer'] == i]
-        cm, score = test_classifier(knnModel, df1)
-        cnf_mats.append(cm)
-    plot_all_cnfmats(cnf_mats)
+    # df = pd.read_csv(TRAINING_FILEPATH+TRAINING_FILE+'.csv')
+    # df = df[feature_list]
+    # """EXCLUDE DANCE MOVES"""
+    # df = df.loc[df['label'] != 12]
+    # cnf_mats = []
+    # for i in range(1,9):
+    #     print("Testing on excluded dancer:", i)
+    #     df1 = df.loc[df['dancer'] == i]
+    #     cm, score = test_classifier(knnModel, df1)
+    #     cnf_mats.append(cm)
+    # plot_all_cnfmats(cnf_mats)
 
     df = pd.read_csv(TESTING_FILEPATH+TESTING_FILE+'.csv')
+    df = df.loc[df['label'] != 12]
     df = df[feature_list]
     print("Testing on unseen dancer")
     cm, score = test_classifier(knnModel, df)
@@ -202,36 +203,23 @@ if __name__ == "__main__":
     # activate logger
     log = activate_logger('train_kf', 'log/'+MODEL_FILE+'.log', append=False)
 
-    """ FEATURES FOR MOVES 1 - 5 """
+    """ FEATURES 10 moves - n_neigh = 4"""
     feature_list = [
-    'mean_ax0', 'mean_az0', 'mean_ax1', 'mean_abs_ax0', 'mean_abs_az0',
-    'mean_abs_az1', 'std_ax0', 'std_ay0', 'std_az0', 'std_ax1',
-    'std_ay1', 'std_az1', 'std_gx0', 'std_gy0', 'std_gx1', 'std_gy1',
-    'median_ax0', 'median_ay0', 'median_ax1', 'median_az1', 'mad_ax0',
-    'mad_ay0', 'mad_az0', 'mad_ax1', 'mad_ay1', 'mad_az1', 'mad_gy0',
-    'mad_gx1', 'mad_gy1', 'max_ax0', 'max_az0', 'max_ay1', 'max_az1',
-    'max_gy0', 'max_gx1', 'min_ay0', 'min_ax1', 'min_az1', 'min_gy0',
-    'min_gx1', 'range_az0', 'range_ax1', 'range_ay1', 'range_az1',
-    'range_gy0', 'range_gx1', 'corr_ayz0', 'corr_axz1', 'corr_ayz1',
-    'corr_gxy0', 'corr_ax1x0', 'corr_ax1y0', 'corr_ax1z0', 'corr_ay1x0',
-    'corr_ay1y0', 'corr_ay1z0', 'corr_gy0y1',
-    'label','dancer','collection']
-    """ FEATURES 10 moves """
-    feature_list = [
-    'mean_ax0', 'mean_az0', 'mean_ax1', 'mean_az1', 'mean_gy1',
-    'mean_abs_ax0', 'mean_abs_ay0', 'mean_abs_az0', 'mean_abs_ax1',
-    'mean_abs_az1', 'mean_abs_gx1', 'mean_abs_gy1', 'std_ax0',
-    'std_ay0', 'std_az0', 'std_ax1', 'std_ay1', 'std_az1', 'std_gx0',
-    'std_gy0', 'std_gx1', 'std_gy1', 'median_ax0', 'median_az0',
-    'median_ax1', 'median_az1', 'median_gy1', 'mad_ax0', 'mad_ay0',
-    'mad_az0', 'mad_ax1', 'mad_ay1', 'mad_az1', 'mad_gx0', 'mad_gy0',
-    'mad_gx1', 'mad_gy1', 'max_ax0', 'max_ay0', 'max_az0', 'max_ax1',
-    'max_az1', 'max_gx0', 'max_gy0', 'max_gy1', 'min_ax0', 'min_ay0',
-    'min_az0', 'min_ax1', 'min_az1', 'min_gx0', 'min_gy0', 'min_gx1',
-    'min_gy1', 'range_ax0', 'range_ay0', 'range_az0', 'range_ax1',
+    'mean_ax0', 'mean_az0', 'mean_ax1', 'mean_az1', 'mean_gx1',
+    'mean_gy1', 'mean_abs_ax0', 'mean_abs_az0', 'mean_abs_ax1',
+    'mean_abs_ay1', 'mean_abs_az1', 'mean_abs_gx1', 'mean_abs_gy1',
+    'std_ax0', 'std_ay0', 'std_az0', 'std_ax1', 'std_ay1', 'std_az1',
+    'std_gx0', 'std_gy0', 'std_gx1', 'std_gy1', 'median_ax0',
+    'median_az0', 'median_ax1', 'median_az1', 'median_gy1', 'mad_ax0',
+    'mad_ay0', 'mad_az0', 'mad_ax1', 'mad_ay1', 'mad_az1', 'mad_gx0',
+    'mad_gy0', 'mad_gx1', 'mad_gy1', 'max_ax0', 'max_az0', 'max_ax1',
+    'max_ay1', 'max_az1', 'max_gx0', 'max_gy0', 'max_gy1', 'min_ax0',
+    'min_ay0', 'min_az0', 'min_ax1', 'min_az1', 'min_gx0', 'min_gy0',
+    'min_gx1', 'min_gy1', 'range_ay0', 'range_az0', 'range_ax1',
     'range_ay1', 'range_az1', 'range_gx0', 'range_gy0', 'range_gx1',
     'range_gy1', 'corr_axz0', 'corr_ayz0', 'corr_axz1', 'corr_gxy0',
-    'corr_ax1x0', 'corr_ay1x0', 'corr_ay1y0', 'corr_ay1z0', 'corr_az1x0',
+    'corr_gxy1', 'corr_ax1x0', 'corr_ay1x0', 'corr_ay1y0', 'corr_ay1z0',
+    'corr_az1x0', 'corr_gx0x1',
     'label','dancer','collection']
 
     """ GET ALL COLUMNS """
@@ -241,14 +229,16 @@ if __name__ == "__main__":
     # df = df2.loc[df2['dancer'] != EXC_DANCER]
 
     """ EXCLUDING SOME LABELS """
-    # df = df2.loc[df2['label'] < 6]
-    df = df2
+    df = df2.loc[df2['label'] != 12]
+    # df = df2
 
     """ REMOVE UNDESIRED COLUMNS """
     df = df[feature_list]
     print("Number of features", len(df.columns))
-
+    res = pd.DataFrame()
     neighbors = range(1,20)
+    # weight_options = ['uniform', 'distance']
+    # algorithm = ['auto', 'ball_tree', 'kd_tree', 'brute']
     scores = []
     n_neigh = []
     for k in neighbors:
@@ -257,8 +247,9 @@ if __name__ == "__main__":
         scores.append(score)
         n_neigh.append(k)
 
-    res['scores'] = time_ms
-    res['n_neighbors'] = df.index
+    res['scores'] = scores
+    res['n_neighbors'] = n_neigh
+    res.to_csv('result-training.csv', index=False)
     f, ax = plt.subplots( 1 )
     sns.pointplot(
         x="n_neighbors",
